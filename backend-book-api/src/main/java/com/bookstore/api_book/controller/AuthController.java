d4027047bd9d15e403f9a1a8bc4530170f711a99
Texhnolyze47 <ichigobankai136@gmail.com>
2024-10-17 21:47:38 -0600

    feat: added error handling to user registration

--- backend-book-api/src/main/java/com/bookstore/api_book/controller/AuthController.java
+++ backend-book-api/src/main/java/com/bookstore/api_book/controller/AuthController.java
@@ -2,20 +2,15 @@ package com.bookstore.api_book.controller;
 
 import com.bookstore.api_book.dto.LoginRequest;
 import com.bookstore.api_book.dto.RegisterRequest;
-import com.bookstore.api_book.model.User;
 import com.bookstore.api_book.repository.UserRepository;
 import com.bookstore.api_book.service.AuthService;
 import com.bookstore.api_book.service.JwtService;
-import com.bookstore.api_book.service.UserService;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,10 +20,6 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AuthController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final JwtService jwtService;
     private final AuthService authService;
 
 
@@ -42,24 +33,18 @@ public class AuthController {
             System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email/password supplied");
         }
-
-
-        String token = jwtService.generateToken(loginRequest.email());
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        
-
-        System.out.println(authentication.getPrincipal()+ " logged in");
-     
-        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
-        
-        authService.registerUser(request);
+        try {
+            authService.registerUser(request);
+            return ResponseEntity.ok("User registered successfully");
+        } catch (Exception e) {
+            //TODO: make exception
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
 
-        return ResponseEntity.ok("User registered successfully");
     }
 }
