3b1abd5b948a8b2f99cc9f8cac1174bc12a59e95
jcrp_2005@hotmail.com <jcrp_2005@hotmail.com>
2024-10-26 16:30:10 -0500

    Create first components and functionalities

--- library-frontend-store/src/App.tsx
+++ library-frontend-store/src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Log from './pages/Login';
+import logo from './assets/logoBliblidigital.svg';
 import './App.css';
 
-function App() {
+const LandingPage: React.FC = () =>{
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Gestionar tus libros nuca fue tan facil!.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link
+          to = "/home"
+          className='app-link'
+          >
+            Ingresar
+          </Link>
       </header>
-    </div>
+    </div>  
+  )
+}
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='login' element={<Log />} />
+      </Routes>
+    </Router>
   );
 }
 
